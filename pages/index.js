41e0922472684b2eb74e6f8936fff06c4c2f3355
nikolalsvk <nikolaseap@gmail.com>
2018-10-15 19:55:45 +0200

    Style and deploy

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,28 @@ import Link from 'next/link'
 import Layout from '../components/layout'
 import fetch from 'isomorphic-unfetch'
 
-const PostLink = (props) => (
-  <li>
-    <Link
-      as={`/p/${props.id}`} 
-      href={`/post?title=${props.title}`}
-    >
-      <a>{props.title}</a>
+const PostLink = ({show}) => (
+  <li key={show.id}>
+    <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+      <a>{show.name}</a>
     </Link>
+
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: "Arial";
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </li>
 )
 
@@ -18,13 +32,33 @@ const Index = (props) => (
     <h1>Batman TV Shows</h1>
     <ul>
       {props.shows.map(({show}) => (
-        <li key={show.id}>
-          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-            <a>{show.name}</a>
-          </Link>
-        </li>
+        <PostLink show={show} />
       ))}
     </ul>
+
+    <style jsx>{`
+      h1, a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </Layout>
 )
 
--- pages/post.js
+++ pages/post.js
@@ -1,12 +1,45 @@
-import {withRouter} from 'next/router'
 import Layout from '../components/layout'
 import fetch from 'isomorphic-unfetch'
+import Markdown from 'react-markdown'
 
 const Post = (props) => (
   <Layout>
     <h1>{props.show.name}</h1>
+    <div className="markdown">
+      <Markdown source={`
+        This is our blog post.
+        Yes. We can have a [link](/link).
+        And we can have a title as well.
+
+        ### This is a title
+
+        And here's the content.
+      `}
+      />
+    </div>
     <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
     <img src={props.show.image.medium}/>
+
+    <style jsx global>{`
+      .markdown {
+        font-family: 'Arial';
+      }
+
+      .markdown a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      .markdown a:hover {
+        opacity: 0.6;
+      }
+
+      .markdown h3 {
+        margin: 0;
+        padding: 0;
+        text-transform: uppercase;
+      }
+    `}</style>
   </Layout>
 )
 
